8eba138b0dcc07bbccd68538c23909665ac6c0af
Ivan Sanchez <ivan.sanchez@mybrana.com>
2018-08-01 16:42:51 +0200

    added simple offline data augmentation

--- train.py
+++ train.py
@@ -2,6 +2,21 @@
 import numpy as np
 import pdb
 import argparse
+from scipy.ndimage import rotate
+import random
+import time, sys
+
+def update_progress(job_title, progress):
+    length = 20 # modify this to change the length
+    block = int(round(length*progress))
+    msg = "\r{0}: [{1}] {2}%".format(job_title, "#"*block + "-"*(length-block), round(progress*100, 2))
+    if progress >= 1: msg += " DONE\r\n"
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+
+
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-X",
@@ -36,8 +51,52 @@ from sklearn.cross_validation import StratifiedShuffleSplit
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+def get_random_rotation(angle):
+    return random.uniform(-angle, angle)
+
+def apply_random_rotation_fn(image, angle):
+    rot = get_random_rotation(angle)
+    return rotate(image, rot, reshape=False)
+
+def apply_horizontal_flip_fn(image):
+    return np.fliplr(image)
+
+def data_augmentation(features,
+                      labels,
+                      n_augmentations_per_image=5,
+                      apply_horizontal_flip=True,
+                      horizontal_flip_chance=1.,
+                      apply_random_rotations=True,
+                      rotation_chance=1.,
+                      max_rotation_angle=20):
+    aux_features = features[:]
+    aux_labels = labels[:]
+
+    assert (features.shape[0] == labels.shape[0])
+
+    for i in range(labels.shape[0]):
+        for _ in range(n_augmentations_per_image):
+            augmented_image = features[i].reshape((50, 37))
+            
+
+            if (random.uniform(0, 1) < rotation_chance):
+                # print('deb')
+                augmented_image = apply_random_rotation_fn(augmented_image, max_rotation_angle)
 
 
+            if (random.uniform(0, 1) < horizontal_flip_chance):
+                # pdb.set_trace()
+                # print('deb2')
+                augmented_image = apply_horizontal_flip_fn(augmented_image)
+            
+            aux_features = np.vstack([aux_features, np.asarray([augmented_image.flatten()])])
+            aux_labels = np.append(aux_labels, labels[i]) # same label obv
+
+            update_progress("Performing data augmentation", i/labels.shape[0])
+    update_progress("Performing data augmentation", 1)
+    
+    return aux_features, aux_labels
+    
 
 
 def cnn_model_fn(features, labels, mode):
@@ -45,9 +104,10 @@ def cnn_model_fn(features, labels, mode):
   # Input Layer
   input_layer = tf.reshape(features["x"], [-1, 50, 37, 1])
 
+  bn_conv_0 = tf.layers.batch_normalization(inputs=input_layer)
   # Convolutional Layer #1
   conv1 = tf.layers.conv2d(
-      inputs=input_layer,
+      inputs=bn_conv_0,
       filters=64,
       kernel_size=[5, 5],
       padding="same",
@@ -56,37 +116,41 @@ def cnn_model_fn(features, labels, mode):
   # Pooling Layer #1
   pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
+  bn_conv_1 = tf.layers.batch_normalization(inputs=pool1)
+
   # Convolutional Layer #2 and Pooling Layer #2
   conv2 = tf.layers.conv2d(
-      inputs=pool1,
+      inputs=bn_conv_1,
       filters=128,
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.leaky_relu)
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
+  bn_conv_2 = tf.layers.batch_normalization(inputs=pool2)
   # Convolutional Layer #3 and Pooling Layer #3
   conv3 = tf.layers.conv2d(
-      inputs=pool2,
+      inputs=bn_conv_2,
       filters=256,
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.leaky_relu)
   pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
 
-  # Dense Layer #1
+  # Flatten layer
   pool3_flat = tf.reshape(pool3, [-1, 6 * 4 * 256])
 
   bn1 = tf.layers.batch_normalization(inputs=pool3_flat)
+  # Dense Layer #1
   dense1 = tf.layers.dense(inputs=bn1, units=1024, activation=tf.nn.leaky_relu)
 
   dropout1 = tf.layers.dropout(
       inputs=dense1, rate=0.7, training=mode == tf.estimator.ModeKeys.TRAIN)
 
   
-  bn1 = tf.layers.batch_normalization(inputs=dropout1)
+  bn2 = tf.layers.batch_normalization(inputs=dropout1)
   # Dense Layer #2
-  dense2 = tf.layers.dense(inputs=bn1, units=1024, activation=tf.nn.leaky_relu)
+  dense2 = tf.layers.dense(inputs=bn2, units=1024, activation=tf.nn.leaky_relu)
   
 
   dropout2 = tf.layers.dropout(
@@ -145,7 +209,9 @@ def main(unused_argv):
         X_test  = X[test_idx]
         y_test  = y[test_idx]
         
+    X_train, y_train = data_augmentation(X_train, y_train, n_augmentations_per_image=1)
 
+    pdb.set_trace()
     # Validate that data is consistent. Each row: one image with a class
     assert (X_train.shape[0] == y_train.shape[0])
 
